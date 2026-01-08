481dee3f95ba486e61fd787f80d373cab677e1ce
LianisDevs <lianisdevs@gmail.com>
2026-01-08 23:13:40 +1100

    add: handle errors with stderr not stdout

--- 0-simple_shell.c
+++ 0-simple_shell.c
@@ -60,7 +60,7 @@ int main(void)
 			
 				if (find_path_result == 1)
 				{
-					printf("./hsh: 1: %s: not found\n", argv->args[0]);
+					fprintf(stderr, "./hsh: 1: %s: not found\n", argv->args[0]);
 					cleanup_argv(argv);
 					cleanup_command_queue(command_queue);
 					free(line);
